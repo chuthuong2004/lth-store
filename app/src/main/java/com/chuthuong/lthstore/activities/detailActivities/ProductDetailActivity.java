43dce94cfe3633002613bdeb68523ce55ab0c96c
chuthuong2004 <19h1120035@sv.ut.edu.vn>
2022-05-13 00:43:16 +0700

    update product detail

--- app/src/main/java/com/chuthuong/lthstore/activities/detailActivities/ProductDetailActivity.java
+++ app/src/main/java/com/chuthuong/lthstore/activities/detailActivities/ProductDetailActivity.java
@@ -2,24 +2,48 @@ package com.chuthuong.lthstore.activities.detailActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.chuthuong.lthstore.R;
 import com.chuthuong.lthstore.model.Product;
+import com.chuthuong.lthstore.model.ProductDetail;
+import com.chuthuong.lthstore.model.ProductImage;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDetailActivity extends AppCompatActivity {
-    ImageView imgDetailProduct;
-    TextView rating, name, description, price;
+    TextView rating, name, description,currentPrice, price, discount, quantitySold, likeCount;
     Button btnAddToCart, btnBuyNow;
     ImageView imgAddToCart, imgRemoveItemCart;
+    RatingBar ratingBar;
 
+    LinearLayout linearLayoutSize;
     // Product
     Product product = null;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +53,82 @@ public class ProductDetailActivity extends AppCompatActivity {
         final Product obj = (Product) getIntent().getSerializableExtra("product_detail");
         if (obj instanceof Product) {
             product = (Product) obj;
+
         }
         Log.e("obj","Ok");
         if(product!=null){
-            Glide.with(getApplicationContext()).load(product.getImages().get(0).getImg()).into(imgDetailProduct);
+            List<ProductImage> productImages = product.getImages();
+            // image slider
+            ImageSlider imageSlider = findViewById(R.id.image_slider_detail);
+            List<SlideModel> slideModelList = new ArrayList<>();
+            for (int i = 0; i < productImages.size(); i++) {
+                slideModelList.add(new SlideModel(productImages.get(i).getImg(), ScaleTypes.CENTER_CROP));
+            }
+            imageSlider.setImageList(slideModelList);
+
             name.setText(product.getName());
-            description.setText(product.getDesProduct());
-            price.setText(product.getPrice()+"");
+//            description.setText(product.getDesProduct());
             rating.setText(product.getRate()+"");
+            discount.setText("-"+product.getDiscount()+"%");
+
+            ratingBar.setRating(product.getRate());
+            NumberFormat formatter = new DecimalFormat("#,###");
+            String formatterPriceProduct = formatter.format(product.getPrice() - (product.getPrice() * product.getDiscount() / 100));
+            price.setText(formatterPriceProduct + "đ");
+            likeCount.setText(product.getLikeCount() + "");
+            quantitySold.setText(product.getQuantitySold() + "");
+            if (product.getDiscount() != 0) {
+                String formatterCurrentPriceProduct = formatter.format(product.getPrice());
+               currentPrice.setText(formatterCurrentPriceProduct + "đ");
+            } else {
+                currentPrice.setText("");
+            }
+            List<ProductDetail> details = product.getDetail();
+            Log.e("size",details.toString());
+            for (int i = 0; i < details.size(); i++) {
+                TextView textView = new TextView(ProductDetailActivity.this);
+                textView.setText(details.get(i).getSize()+"");
+                textView.setBackgroundResource(R.drawable.image_view_bg_circle);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
+                        (100, ViewGroup.LayoutParams.WRAP_CONTENT);
+                params.setMargins(0, 0, 20, 0);
+                textView.setLayoutParams(params);
+                textView.setGravity(Gravity.CENTER);
+                linearLayoutSize.addView(textView);
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @SuppressLint("ResourceType")
+                    @Override
+
+                    public void onClick(View v) {
+                        count++;
+                        if(count>1) {
+                            textView.setTextColor(getResources().getColor(R.color.pink));
+                            textView.setBackgroundResource(R.drawable.image_view_bg_circle);
+                            count = 0;
+                        } else {
+                            textView.setTextColor(getResources().getColor(R.color.white));
+                            textView.setBackgroundResource(R.drawable.image_view_bg_circle_pink);
+
+                        }
+                    }
+                });
+            }
+            Log.e("size", product.getDetail().size()+"");
         }
     }
 
     private void addControls() {
-        imgDetailProduct = findViewById(R.id.detail_product_img);
         rating = findViewById(R.id.detail_product_rating);
         name = findViewById(R.id.detail_product_name);
-        description = findViewById(R.id.detail_description_product);
+//        description = findViewById(R.id.detail_description_product);
         price = findViewById(R.id.detail_product_price);
-        btnAddToCart = findViewById(R.id.add_to_cart);
-        btnBuyNow = findViewById(R.id.buy_now);
-        imgAddToCart = findViewById(R.id.add_quantity_detail);
-        imgRemoveItemCart = findViewById(R.id.remove_quantity_detail);
+        currentPrice = findViewById(R.id.detail_product_current_price);
+        quantitySold = findViewById(R.id.detail_product_sold);
+        likeCount = findViewById(R.id.detail_product_favorite);
+        discount = findViewById(R.id.detail_product_discount);
+        ratingBar =findViewById(R.id.detail_product_rating_bar);
+        linearLayoutSize = findViewById(R.id.linear_layout_size);
+
+
     }
 }
